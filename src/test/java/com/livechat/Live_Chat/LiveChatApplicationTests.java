6e01ca72d90204f32cc29572e1a2cd4faadc28f6
xogh8755 <xogh8755@naver.com>
2023-02-24 17:09:13 +0900

    teho connect h2

--- src/test/java/com/livechat/Live_Chat/LiveChatApplicationTests.java
+++ src/test/java/com/livechat/Live_Chat/LiveChatApplicationTests.java
@@ -1,13 +1,22 @@
 package com.livechat.Live_Chat;
 
+import com.livechat.Live_Chat.member.MemberRepository;
+import com.livechat.Live_Chat.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LiveChatApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MemberService m;
+    private MemberRepository mr;
+
+    @Test
+    void testH2() {
+        m.create("teho1", "12324", "qqwe");
+
+    }
 
 }
